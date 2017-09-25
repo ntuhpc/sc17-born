77cdcdb9b62050ae940cdc628906fda732edf5cf
SHAO YIYANG <shaoyiyang312@hotmail.com>
2017-09-25 19:58:00 +0800

    build script

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+BORN_INSTALL_DIR=/home/public/born/install
 rm -rf build
 mkdir build
 cd build
-export CXX=g++
-cmake -DgenericIO_DIR=/sep/bob/genericIO/noSEP/lib -DCMAKE_INSTALL_PREFIX=/sep/bob/Born2 ..
-make
-make install
+CXX=icpc CC=icc cmake -DCMAKE_LIBRARY_PATH=$TBBROOT/lib/intel64/gcc4.7 -DgenericIO_DIR=$BORN_INSTALL_DIR/lib -Dhypercube_DIR=$BORN_INSTALL_DIR/lib -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-xHost" -DUSE_GPU=ON .. 
+make -j 
